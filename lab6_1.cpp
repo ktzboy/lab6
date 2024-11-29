f9c09cb8ac3182c1e4159bb10e62fbd82451e044
ktzboy <pongpol.sritalanut@gmail.com>
2024-11-29 15:38:32 +0700

    Filled out README

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int input, even = 0, odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> input;
+    
+    while(input != 0) {
+        if(input % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+        cout << "Enter an integer: ";
+        cin >> input;
+    }
+    
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
